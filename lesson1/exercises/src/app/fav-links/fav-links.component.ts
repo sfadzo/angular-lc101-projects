6b1fc576ec037f30f2044305da5690099b4e97d0
Paul Rodriguez <paul_rodriguez@comcast.com>
2022-08-01 15:58:00 -0600

    3rd commit

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinks: string[]=['https://www.google.com/', 'https://www.espn.com/'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
