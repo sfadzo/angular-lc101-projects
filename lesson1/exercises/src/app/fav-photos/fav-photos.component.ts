e67b8d61c8a613550b389dadc0a640360ffcdb77
Paul Rodriguez <paul_rodriguez@comcast.com>
2022-08-01 14:01:17 -0600

    1st commit exercise

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Rad Pics';
+  image1 = 'https://www.dropbox.com/s/0ixszef9spr7yh8/gfB0bV3GVqQ4XVeBVwNrjph9DNh4UzQFUzdB2xOvALa8H0PbG1sMGR4ybBq9ziDqD2Wgep-4gNGUrvcO4vzyOyahpFH60leip7ijcYYXxs7n6KXiVZE5%3Dw1600-1001046246.png?dl=1';
+  image2 = 'https://cdn.shopify.com/s/files/1/0057/3728/3618/products/20664117398ad7301d9a9af6d2e5aa5c_1e3ea98b-b962-4982-9f74-2e44381fc6ff_480x.progressive.jpg?v=1573618694';
+  image3 = 'https://m.media-amazon.com/images/M/MV5BMTcxNDM3OTMzOV5BMl5BanBnXkFtZTcwODYxMDIyNw@@._V1_.jpg';
 
   constructor() { }
 
